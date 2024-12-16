79a124c59bc8c878f70e096e26ade467d0574ac1
ASK-Baloch <balochsaeed888@gmail.com>
2024-12-16 22:59:17 +0500

    taking out nodemodules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hellow");
+import express from 'express';
+
+
+
+const app = express();
+
+
+
+app.get('/', (req,res) => {
+    res.send("helloworld")
+})
+app.listen(3000, ()=> {
+    console.log('Server running on http://localhost:3000/');
+})
+
